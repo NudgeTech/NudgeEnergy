b5b6bad52895466d5b78b11b0c5e9c5c6b7dbd67
NudgeTech <mikecrooks83@gmail.com>
2015-12-31 15:44:50 +0000

    default admin & acl

--- server/boot/script.js
+++ server/boot/script.js
@@ -3,24 +3,72 @@ module.exports = function(app) {
   var Role = app.models.Role;
   var RoleMapping = app.models.RoleMapping;
 
+   User.findOne({
+    email: 'admin@admin.com'
+  }, function(err, user) {
 
-    //create the admin role
-    Role.create({
-      name: 'admin'
-    }, function(err, role) {
-      if (err) throw err;
+      if(!user){
+          
+          console.log ("user does not exisit");
+          var newUser = new User();
 
-      console.log('Created role:', role);
+          newUser.username = 'admin';
+          newUser.password = 'password';
+          newUser.email = 'admin@admin.com';
+          newUser.id = '101';
+      
+          newUser.save(function(err) {
+      
+              if(!err) {
+                //res.status(status).send(body)
+               console.log("User created with name: " + newUser.username);
+      
+              } else {
+                  console.log("failed");
+              }
+          });
 
-      //make user an admin
-      role.principals.create({
-        principalType: RoleMapping.USER,
-        principalId: '56816186d08e9604d37869c2'
-      }, function(err, principal) {
-        if (err) throw err;
 
-        console.log('Created principal:', principal);
-      });
-    });
+           //create the admin role
+          Role.create({
+             name: 'admin'
+                }, function(err, role) {
+                  if (err) throw err;
+
+                  console.log('Created role:', role);
+
+                  //make user an admin
+                  role.principals.create({
+                    principalType: RoleMapping.USER,
+                    principalId: newUser.id
+                  }, function(err, principal) {
+                    if (err) throw err;
+
+                    console.log('Created principal:', principal);
+                  });
+              });  
+
+
+
+      }else if (user){
+
+           console.log ("user does exisit and is already an admin");
+           console.log (user.id);
+        
+
+      }
+
+
+  });
+          
+       
+            
+        
+
+  
+
+
+
+   
 
 }
