84cf9818d4f745c96d6bd14b1c1b00bec801af0d
NudgeTech <mikecrooks83@gmail.com>
2016-01-03 13:51:25 +0000

    added nudge complete remote method

--- common/models/nudge.js
+++ common/models/nudge.js
@@ -1,3 +1,59 @@
 module.exports = function(Nudge) {
 
+	Nudge.observe('before save', function (ctx, next) {
+		  if (ctx.instance) {
+		    ctx.instance.date_created = new Date();
+		  } else {
+		    ctx.data.date_created = new Date();
+		  }
+		  next();
+	});
+
+	Nudge.complete = function(nudgeID, cb){
+		var response;
+		var nudgeID = nudgeID;
+
+		//find the nudge to update
+		Nudge.findById(nudgeID, function(err, nudge) {
+	  			if(!nudge){
+	  				console.log(nudgeID);
+	  				console.log ("This Nudge has been removed or does not exisit");
+	  			}else{
+	  				nudge.updateAttributes({
+	  					id: nudge.id,
+	  					complete: true
+	  				}, function(err) {
+		  					console.log("error check");
+		  					if(err){
+		  						console.log("error occured");
+		  					}else{
+
+		  						console.log("no error");	  						
+		  					}
+
+	  				});
+
+  			}
+
+  		});
+		
+		cb(null, response);
+	};
+
+	//expose the method through REST
+    Nudge.remoteMethod('complete', {
+        accepts: {
+            arg: 'nudgeID',
+            type: 'string'
+        },
+        returns: {
+            arg: 'response',
+            type: 'string'
+        },
+        http: {
+            path: '/complete',
+            verb: 'post'
+        }
+    });
+
 };
